1591a22ad9dc269e8daed43062af0216b6148472
kure3rd <kure3rd@gmail.com>
2022-04-30 12:52:17 +0900

    とりあえず動いた

--- Program.cs
+++ Program.cs
@@ -3,33 +3,44 @@ using System.Runtime.Remoting;
 using System.Runtime.Remoting.Channels;
 using System.Runtime.Remoting.Channels.Ipc;
 
+namespace FNF.Utility {
+    class BouyomiChanRemoting : MarshalByRefObject {
+        public void AddTalkTask (string sTalkText) { }
+        public void AddTalkTask (string sTalkText, int iSpeed, int iVolume, int vType) { AddTalkTask(sTalkText); }
 
-public class BouyomiChanRemoting : MarshalByRefObject {
-    public void AddTalkTask (string sTalkText) { Console.WriteLine('A'); }
-    public void AddTalkTask (string sTalkText, int iSpeed, int iVolume, int vType) { Console.WriteLine('B'); }
+        public void AddTalkTask (string sTalkText, int iSpeed, int iTone, int iVolume, int vType) { AddTalkTask(sTalkText); }
+        public int  AddTalkTask2(string sTalkText) { return 0; }
+        public int  AddTalkTask2(string sTalkText, int iSpeed, int iTone, int iVolume, int vType) { AddTalkTask2(sTalkText); return 0; }
+        public void ClearTalkTasks() { Console.WriteLine("A"); }
+        public void SkipTalkTask() { Console.WriteLine("A"); }
 
-    public void AddTalkTask (string sTalkText, int iSpeed, int iTone, int iVolume, int vType) { Console.WriteLine('C'); }
-    public int  AddTalkTask2(string sTalkText) { Console.WriteLine('D'); return 0; }
-    public int  AddTalkTask2(string sTalkText, int iSpeed, int iTone, int iVolume, int vType) { Console.WriteLine('E'); return 0; }
-    public void ClearTalkTasks() { }
-    public void SkipTalkTask() { }
-
-    public int  TalkTaskCount { get { return 0; }         }
-    public int  NowTaskId     { get { return 0; }         }
-    public bool NowPlaying    { get { return true; }         }
-    public bool Pause         { get { return false; } set { } }
+        public int  TalkTaskCount { get { return 0; }         }
+        public int  NowTaskId     { get { return 0; }         }
+        public bool NowPlaying    { get { return false; }         }
+        public bool Pause         { get { return false; } set { } }
+    }
 }
 
-
 class Program
-{    static void Main()
+{    
+    static void Main()
     {
         var ServerChannel = new IpcServerChannel("BouyomiChan");
         ChannelServices.RegisterChannel(ServerChannel, false);
+        var RemotingObject = new FNF.Utility.BouyomiChanRemoting();
+        RemotingServices.Marshal(RemotingObject, "Remoting", typeof(FNF.Utility.BouyomiChanRemoting));
 
-        var RemotingObject = new BouyomiChanRemoting();
-        RemotingServices.Marshal(RemotingObject, "Remoting", typeof(BouyomiChanRemoting));
-        
+        var ClientChannelA = new IpcClientChannel("hogeA", null); //チャンネル名は何でもいい
+        ChannelServices.RegisterChannel(ClientChannelA, false);
+        var RemotingObjectA = (FNF.Utility.BouyomiChanRemoting)Activator.GetObject(typeof(FNF.Utility.BouyomiChanRemoting), "ipc://BouyomiChanA/Remoting");
+        RemotingObjectA.AddTalkTask2("test A");
+
+        var ClientChannelB = new IpcClientChannel("hogeB", null); //チャンネル名は何でもいい
+        ChannelServices.RegisterChannel(ClientChannelB, false);
+        var RemotingObjectB = (FNF.Utility.BouyomiChanRemoting)Activator.GetObject(typeof(FNF.Utility.BouyomiChanRemoting), "ipc://BouyomiChanB/Remoting");
+        RemotingObjectB.AddTalkTask2("test B");
+    
+ 
         Console.WriteLine("Hello, World!");
         Console.WriteLine(ServerChannel.GetChannelUri());
         Console.ReadLine();
