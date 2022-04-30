9b462c944a157b93517ab4268ea7ffdc691c7853
kure3rd <kure3rd@gmail.com>
2022-04-30 13:57:15 +0900

    add event

--- Program.cs
+++ Program.cs
@@ -2,14 +2,17 @@ using System;
 using System.Runtime.Remoting;
 using System.Runtime.Remoting.Channels;
 using System.Runtime.Remoting.Channels.Ipc;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace FNF.Utility {
     class BouyomiChanRemoting : MarshalByRefObject {
-        public void AddTalkTask (string sTalkText) { }
+        public event EventHandler TalkTextEvent;
+        public void AddTalkTask (string sTalkText) { TalkTextEvent(this, EventArgs.Empty); }
         public void AddTalkTask (string sTalkText, int iSpeed, int iVolume, int vType) { AddTalkTask(sTalkText); }
 
         public void AddTalkTask (string sTalkText, int iSpeed, int iTone, int iVolume, int vType) { AddTalkTask(sTalkText); }
-        public int  AddTalkTask2(string sTalkText) { return 0; }
+        public int  AddTalkTask2(string sTalkText) { TalkTextEvent(this, EventArgs.Empty); return 0; }
         public int  AddTalkTask2(string sTalkText, int iSpeed, int iTone, int iVolume, int vType) { AddTalkTask2(sTalkText); return 0; }
         public void ClearTalkTasks() { Console.WriteLine("A"); }
         public void SkipTalkTask() { Console.WriteLine("A"); }
@@ -21,28 +24,54 @@ namespace FNF.Utility {
     }
 }
 
-class Program
-{    
-    static void Main()
+class BouyomiChanServer
+{
+    FNF.Utility.BouyomiChanRemoting RemotingObjectA;
+    FNF.Utility.BouyomiChanRemoting RemotingObjectB;
+
+    void Talk(object sender, System.EventArgs e)
+    {
+        Console.WriteLine("TalkText Called");
+        RemotingObjectA.AddTalkTask2("test A");
+        RemotingObjectB.AddTalkTask2("test B");
+    }
+
+    public BouyomiChanServer(FNF.Utility.BouyomiChanRemoting RemotingObjectA, FNF.Utility.BouyomiChanRemoting RemotingObjectB)
     {
         var ServerChannel = new IpcServerChannel("BouyomiChan");
         ChannelServices.RegisterChannel(ServerChannel, false);
         var RemotingObject = new FNF.Utility.BouyomiChanRemoting();
+        RemotingObject.TalkTextEvent += new EventHandler(Talk);
         RemotingServices.Marshal(RemotingObject, "Remoting", typeof(FNF.Utility.BouyomiChanRemoting));
 
-        var ClientChannelA = new IpcClientChannel("hogeA", null); //チャンネル名は何でもいい
-        ChannelServices.RegisterChannel(ClientChannelA, false);
-        var RemotingObjectA = (FNF.Utility.BouyomiChanRemoting)Activator.GetObject(typeof(FNF.Utility.BouyomiChanRemoting), "ipc://BouyomiChanA/Remoting");
-        RemotingObjectA.AddTalkTask2("test A");
+        this.RemotingObjectA = RemotingObjectA;
+        this.RemotingObjectB = RemotingObjectB;
+    }
+}
 
-        var ClientChannelB = new IpcClientChannel("hogeB", null); //チャンネル名は何でもいい
-        ChannelServices.RegisterChannel(ClientChannelB, false);
-        var RemotingObjectB = (FNF.Utility.BouyomiChanRemoting)Activator.GetObject(typeof(FNF.Utility.BouyomiChanRemoting), "ipc://BouyomiChanB/Remoting");
-        RemotingObjectB.AddTalkTask2("test B");
-    
- 
+class BouyomiChanClient
+{
+    public FNF.Utility.BouyomiChanRemoting RemotingObject;
+    public BouyomiChanClient(string IpcChannelName)
+    {
+        var ClientChannel = new IpcClientChannel(IpcChannelName, null); //チャンネル名は何でもいい
+        ChannelServices.RegisterChannel(ClientChannel, false);
+        RemotingObject = (FNF.Utility.BouyomiChanRemoting)Activator.GetObject(typeof(FNF.Utility.BouyomiChanRemoting), "ipc://"+IpcChannelName+"/Remoting");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
         Console.WriteLine("Hello, World!");
-        Console.WriteLine(ServerChannel.GetChannelUri());
+
+        var ClientA = new BouyomiChanClient("BouyomiChanA");
+        var ClientB = new BouyomiChanClient("BouyomiChanB");
+
+        var Server = new BouyomiChanServer(ClientA.RemotingObject, ClientB.RemotingObject);
+
+//        Console.WriteLine(ServerChannel.GetChannelUri());
         Console.ReadLine();
     }
 }
