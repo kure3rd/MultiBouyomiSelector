f4af3ed281138a1830cbf378857e5cba1abf1605
kure3rd <kure3rd@gmail.com>
2022-05-12 19:12:39 +0900

    include .config & .pdb #15

--- zipping.ps1
+++ zipping.ps1
@@ -7,15 +7,13 @@ Write-Output $outputDir
 if (Test-Path $outputDir) {}
 else {New-Item $outputDir -ItemType Directory}
 
-$targetPath = (Join-Path $cwd "bin\Release\net48\Publish")
-$exePath = (Join-Path $targetPath "*.exe")
-$settingPath = (Join-Path $targetPath "settings.xml")
+$targetPath = (Join-Path $cwd "bin\Debug\net48\*")
 Write-Output $targetPath
 
 $outFileName = "MultiBouyomiSelector.zip"
 $outFilePath = (Join-Path $outputDir $outFileName)
 
-Compress-Archive -Path $exePath,$settingPath -DestinationPath $outFilePath -Force
+Compress-Archive -Path $targetPath -DestinationPath $outFilePath -Force
 
 #Write-Output $outputDir
 #Write-Output $outFilePath
